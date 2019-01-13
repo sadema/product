14f725441e3a9a4d156b3be8ee087fa715d32021
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2019-01-13 20:54:15 +0100

    [develop] added PWD

--- cicd/build/buildcode.sh
+++ cicd/build/buildcode.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-cd m2
 PWD=$(pwd)
 echo "PWD: ${PWD}"
 
-M2_LOCAL_REPO=${PWD}/rootfs/m2
+M2_LOCAL_REPO=${PWD}/m2/rootfs/m2
 echo "M2_LOCAL_REPO: ${M2_LOCAL_REPO}"
 
 MVN_SETTINGS=${M2_LOCAL_REPO}/settings.xml
 echo "MVN_SETTINGS: ${MVN_SETTINGS}"
 
-cd resource-inschrijving-product
+cd ${PWD}/resource-inschrijving-product
 cat ${MVN_SETTINGS}
-#mvn -s ${MVN_SETTINGS} clean package
-#cp -a target/* ../artifacts/
+mvn -s ${MVN_SETTINGS} clean package
+cp -a target/* ../artifacts/
