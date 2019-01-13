e5b5916dc03d5f3920203f0e09ec48ea166536f6
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2019-01-13 21:06:17 +0100

    [develop] dynamic settings.xml

--- cicd/build/buildcode.sh
+++ cicd/build/buildcode.sh
@@ -3,11 +3,20 @@
 PWD=$(pwd)
 echo "PWD: ${PWD}"
 
-M2_LOCAL_REPO=${PWD}/m2/rootfs/m2
-echo "M2_LOCAL_REPO: ${M2_LOCAL_REPO}"
+M2_LOCAL=${PWD}/m2/rootfs/m2
+echo "M2_LOCAL: ${M2_LOCAL}"
 
-MVN_SETTINGS=${M2_LOCAL_REPO}/settings.xml
+MVN_SETTINGS=${M2_LOCAL}/settings.xml
 echo "MVN_SETTINGS: ${MVN_SETTINGS}"
+
+cat > ${MVN_SETTINGS} << EOF
+
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+      <localRepository>${M2_LOCAL}/repository</localRepository>
+</settings>
 
 cd ${PWD}/resource-inschrijving-product
 cat ${MVN_SETTINGS}
