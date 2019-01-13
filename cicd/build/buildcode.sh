bf4aff47b8e59e7d1b4ec0cd441fa55a52598f04
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2019-01-13 20:49:41 +0100

    [develop] test

--- cicd/build/buildcode.sh
+++ cicd/build/buildcode.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-M2_LOCAL_REPO=m2/rootfs/m2
+cd m2
+PWD=$(pwd)
+echo "PWD: ${PWD}"
+
+M2_LOCAL_REPO=${PWD}/rootfs/m2
+echo "M2_LOCAL_REPO: ${M2_LOCAL_REPO}"
+
 MVN_SETTINGS=${M2_LOCAL_REPO}/settings.xml
+echo "MVN_SETTINGS: ${MVN_SETTINGS}"
+
 cd resource-inschrijving-product
-cat ../${MVN_SETTINGS}
+cat ${MVN_SETTINGS}
 #mvn -s ${MVN_SETTINGS} clean package
 #cp -a target/* ../artifacts/
