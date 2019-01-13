fdea99159b88ad18aa17e69f21a2d91572980c83
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2019-01-13 20:35:31 +0100

    [develop] added settings.xml

--- cicd/build/buildcode.sh
+++ cicd/build/buildcode.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cd m2/rootfs/m2
-find .
-#cd resource-inschrijving-product
-#mvn clean package
+M2_LOCAL_REPO=/m2/rootfs/m2
+MVN_SETTINGS=${M2_LOCAL_REPO}/settings.xml
+cd resource-inschrijving-product
+cat ${MVN_SETTINGS}
+#mvn -s ${MVN_SETTINGS} clean package
 #cp -a target/* ../artifacts/
