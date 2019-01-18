b475f4f3e3e8fb6bd1940030b9c4d6cd803dfe9d
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2019-01-18 17:43:52 +0100

    [develop] added debug commands

--- cicd/docker-image/create-docker-build-env.sh
+++ cicd/docker-image/create-docker-build-env.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo $(pwd)
+find ./artifacts
+find ./workspace
+
 cp -a artifacts/* workspace
 cp -a resource-inschrijving-product/cicd/docker-image/Dockerfile workspace
+
+ls -l
+find ./workspace
 
 echo "created docker build environment!"
